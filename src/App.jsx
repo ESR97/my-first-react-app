ea2decc2f8888421a10939e4e18b65df00fa3113
ESR <zzy970911@live.com>
2023-02-06 22:16:21 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import ReportButton from "./components/ReportButton"
+import Counter from "./components/Counter";
 
 function App() {
 
-  const obj = {
-    a: 1,
-    b: 2
-  }
-
-  obj[Symbol.iterator] = function* () {
-    for (let prop in obj) {
-      yield [prop, obj[prop]]
-    }
-  } 
-
   return (
     <div>
-      <ReportButton />
+      <Counter />
     </div>
   )
 }
---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function Counter() {
+  
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(prev => prev + 1)
+    console.log("count update", count)
+  }
+
+  const decrease = () => {
+    setCount(prev => prev - 1);
+  }
+
+  window.getCount = () => {
+    console.log("count",count)
+  }
+
+  return (
+    <div>
+      <button onClick={increase}>+</button>
+      <span>{ count }</span>
+      <button onClick={decrease}>-</button>
+    </div>
+  )
+}
